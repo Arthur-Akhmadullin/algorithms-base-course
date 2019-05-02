9d676741781f30783114a03827a2f0b5d59e2681
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2019-05-02 23:09:36 +0500

    implemented the basic functions of a two-way queue

--- 6_deque/deque.py
+++ 6_deque/deque.py
@@ -1,27 +1,22 @@
 class Deque:
     def __init__(self):
         self.deque = []
-    # инициализация внутреннего хранилища
 
     def addFront(self, item):
-        self.deque.insert(0, item)
-    # добавление в голову
+        self.deque.append(item)
 
     def addTail(self, item):
-        self.deque.append(item)
-    # добавление в хвост
+        self.deque.insert(0, item)
 
     def removeFront(self):
         if self.size() > 0:
-            return self.deque.pop(0)
-        # удаление из головы
-        return None # если очередь пуста
+            return self.deque.pop()
+        return None
 
     def removeTail(self):
         if self.size() > 0:
-            return self.deque.pop()
-        # удаление из хвоста
-        return None # если очередь пуста
+            return self.deque.pop(0)
+        return None
 
     def size(self):
-        return len(self.deque) # размер очереди
+        return len(self.deque)
