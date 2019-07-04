5afcc51c8725bb9877adb3ce5aa8b849b5e325a6
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2019-07-05 00:14:22 +0500

    Class Powerset - fixed methods get and find and class HashTable replaced

--- 11_set/set.py
+++ 11_set/set.py
@@ -1,9 +1,81 @@
 # наследуйте этот класс от HashTable
 # или расширьте его методами из HashTable
-from HashTable import HashTable
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size_slots = sz
+        self.step = stp
+        self.slots = [None] * self.size_slots
+
+
+    def hash_fun(self, value):
+        b = bytearray(str(value).encode('utf-8'))
+        sum = 0
+        for i in range(len(b)):
+            sum += b[i]
+        return sum % self.size_slots
+
+
+    def seek_slot(self, value):
+        hash_index = self.hash_fun(value)
+
+        count_full_slots = 0
+        while True:
+            if count_full_slots == self.size_slots:
+                return None
+            elif self.slots[hash_index] == None:
+                return hash_index
+            else:
+                count_full_slots += 1
+                hash_index = (hash_index + self.step) % self.size_slots
+
+
+    def put(self, value):
+        position = self.seek_slot(value)
+
+        if position == None:
+            return None
+        else:
+            self.slots[position] = value
+            return position
+        # записываем значение по хэш-функции
+
+        # возвращается индекс слота или None,
+        # если из-за коллизий элемент не удаётся
+        # разместить
+
+
+    def find(self, value):
+
+        startslot = self.hash_fun(value)
+
+        find_pos = startslot
+
+        if self.slots[find_pos] == value:
+            return find_pos
+        else:
+            while True:
+                find_pos = (find_pos + self.step) % self.size_slots
+                if self.slots[find_pos] == value:
+                    return find_pos
+                if self.slots[find_pos] == None:
+                    break
+                if find_pos == startslot:
+                    break
+        return None
+
+
+    '''
+        for i in range(len(self.slots)):
+            if self.slots[i] == value:
+                return i
+        # находит индекс слота со значением, или None
+        return None
+    '''
+
+
 
 class PowerSet(HashTable):
-    def __init__(self, size_set=20, step_set=1):
+    def __init__(self, size_set=20000, step_set=1):
         HashTable.__init__(self, size_set, step_set)
         self.powerset = []
         # ваша реализация хранилища
@@ -124,9 +196,10 @@ class PowerSet(HashTable):
             return False
 
         for i in set2.slots:
-            if self.get(i) is False:
+            if not self.get(i):
                 return False
+
+        return True
         # возвращает True, если set2 есть
         # подмножество текущего множества,
         # иначе False
-        return True
