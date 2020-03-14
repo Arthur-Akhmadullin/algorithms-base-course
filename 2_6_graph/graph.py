6624b4acbd51cef8480162341d55a8dbd3153077
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2020-03-14 22:14:44 +0500

    add function DepthFirstSearch to find the shortest path

--- 2_6_graph/graph.py
+++ 2_6_graph/graph.py
@@ -2,6 +2,7 @@ class Vertex:
 
     def __init__(self, val):
         self.Value = val
+        self.Hit = False
 
 class SimpleGraph:
 
@@ -51,3 +52,69 @@ class SimpleGraph:
         if self.vertex[v1] is not None and self.vertex[v2] is not None:
             self.m_adjacency[v1][v2] = 0
             self.m_adjacency[v2][v1] = 0
+
+
+    def DepthFirstSearch(self, VFrom, VTo):
+        stack = []
+        for v in self.vertex:
+            v.Hit = False
+        return self.RecursiveDepthFirstSearch(self.vertex[VFrom], self.vertex[VTo], stack, True)
+        # узлы задаются позициями в списке vertex
+        # возвращается список узлов -- путь из VFrom в VTo
+        # или [] если пути нету
+
+
+
+    def RecursiveDepthFirstSearch(self, VFrom, VTo, stack, flagPushStack):
+        VFrom.Hit = True
+        if flagPushStack is True:
+            stack.append(VFrom)
+
+        if len(stack) > 2:
+            indexHead = self.vertex.index(stack[0])
+            indexTail = self.vertex.index(stack[-1])
+            if self.IsEdge(indexHead, indexTail) is True:
+                stack.clear()
+                stack.append(self.vertex[indexHead])
+                stack.append(self.vertex[indexTail])
+
+        if self.SearchTargetVertex(VFrom, VTo):
+            stack.append(self.SearchTargetVertex(VFrom, VTo))
+            return stack
+        elif self.SearchAdjacentVertex(VFrom):
+            return self.RecursiveDepthFirstSearch(self.SearchAdjacentVertex(VFrom), VTo, stack, True)
+        else:
+            stack.pop()
+            if stack == []:
+                return stack
+            else:
+                #return self.SearchTargetVertex(stack[-1], VTo)
+                return self.RecursiveDepthFirstSearch(stack[-1], VTo, stack, False)
+
+
+
+    def SearchTargetVertex(self, VFrom, VTo):
+        for i in range(len(self.vertex)):
+            if self.IsEdge(self.vertex.index(VFrom), i) is True and \
+                    self.vertex[i] is VTo and self.vertex[i].Hit is False:
+                # ДОБАВИЛ self.vertex[i].Hit is False
+                #stack.append(v)
+                #return stack
+                return self.vertex[i]
+        return None
+
+
+    def SearchAdjacentVertex(self, VFrom):
+        for i in range(len(self.vertex)):
+            if self.IsEdge(self.vertex.index(VFrom), i) is True and \
+                    self.vertex[i].Hit is False:
+                return self.vertex[i]
+        return None
+
+    '''
+        def PopStack(self, stack):
+            stack.pop()
+            if stack == []:
+                return None
+            return stack[-1]
+    '''
